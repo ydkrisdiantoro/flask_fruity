f36c8b0d909ea217f08a8a7a6ce994eabfd3ee6b
Muhammad Wildan Oktavian <oktavianwww@gmail.com>
2021-06-03 15:06:22 +0700

    update kodingan model

--- models/main.py
+++ models/main.py
@@ -14,7 +14,6 @@ api = Api(app)
 def _check(test, data):
     return SequenceMatcher(a=test.lower(), b=data.lower()).ratio()
 
-
 def _check_query(search):
     label = df['name'].values
     temp = []
@@ -22,42 +21,41 @@ def _check_query(search):
     for i in label:
         ratio = _check(search, i)
         if ratio > 0.65:
-            temp.append((index, ratio))
-        index += 1
+            temp.append((index,ratio))
+        index+=1
     print(temp)
-    if len(temp) > 0:
+    if len(temp)>0:
         temp.sort(key=lambda x: x[1], reverse=True)
         return temp[0][0]
     else:
         return 999
-
 
 @app.route('/fruit/<n>/<name>')
 def get(name, n):
     query = _check_query(name)
     print(query)
     if query != 999:
-        X = df.drop(columns=['name', 'energy (kcal/kJ)'])
+        X = df.drop(columns=['name','energy (kcal/kJ)'])
         preds = knn.kneighbors(X, n_neighbors=int(n))[1]
+        
+        arr_idx = preds[query] 
         result = []
-        for i in preds[query]:
-            result.append(df.loc[i].values.tolist())
-
+        for item in df.loc[arr_idx].values.tolist():
+            result.append(dict(zip(df.columns, item)))
         return {
-            'result': json.dumps(result)
+            'result': json.dumps(result,separators=(',', ':'))
         }
-    else:
+    else :
         return {
-            'result': 0
-        }
-
+            'result':0
+        }       
 
 if __name__ == '__main__':
     knn = joblib.load('./model_fruit.joblib')
     df = pd.read_csv('./fruits.csv')
-    df = df.replace('-', 0)
+    df = df.replace('-',0)
     for i in df.columns:
-        if i != 'name' and i != 'energy (kcal/kJ)':
-            df[i] = df[i].astype(float)
+      if i != 'name' and i!= 'energy (kcal/kJ)':
+        df[i] = df[i].astype(float)
 
     app.run(debug=True)
